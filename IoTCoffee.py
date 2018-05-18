b4f8731bf7135e3fe7dd6802fc92632cbc9eb7d8
Demo User <debian@beaglebone.localdomain>
2018-05-18 05:13:01 +0000

    working dm demo

--- IoTCoffee.py
+++ IoTCoffee.py
@@ -8,17 +8,8 @@ import rcpy.motor as motor
 ON ="GP0_6"  # GP0 for relays
 OFF ="GP0_5"
 WET ="P9_23"  # GP0 for buttons
-UP = 0.7
-DOWN = -0.9
 
-servo1 = servo.servo1
 pump = motor.motor3
-
-servo.enable()
-clk= servo1.start(0.02)
-servo1.pulse(UP)
-time.sleep(1)
-servo.disable()
 pump.set(0)
 
 # Set the GPIO pins:
@@ -30,15 +21,16 @@ GPIO.output(ON, 0)
 GPIO.output(OFF, 0)
 
 cfg = { 
-    "consumer_key"        : "ADeBhHXYOgoNXgagWe7ZjRB3C",
-    "consumer_secret"     : "dXzWSyFeFCAzgT7b9llIbG4nQ0YTN69BPnEfR7L9QlGPV4Pjfr",
-    "access_token"        : "991703272636452865-QiM7NZWdo3kMxxmjW3ogkKSGhXGFu5S",
-    "access_token_secret" : "UR7tgzgexIqFFajQw0pSdfZzSXehH4bntauWyzaQ6Wgkm"
+    "consumer_key"        : "VDXOzIU8iMXAxOTGt7fiIZYX0",
+    "consumer_secret"     : "PSkn6GnkqSo3ghZGw0mF4vvtYsEHgShqB24MLJZvre5BumLQFJ",
+    "access_token"        : "991703272636452865-DAyKVHoOhGHsj6gjQTAEUAFdUWiJeDx",
+    "access_token_secret" : "aFSF5ELQ7yRzRtANATVAuJTJqAGx3CXrTSKkj4wfQrHvU"
 }
 
 auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
 auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
 twitter = tweepy.API(auth)
+print("Authenticated", twitter.me().name)
 myID = "991703272636452865"
 tweet = "This is a test tweet: http://adambretsch.com"
 
@@ -47,12 +39,6 @@ tweet = "This is a test tweet: http://adambretsch.com"
 
 def makeCoffee():
   print("Begin makeCoffee()")
-  
-  print("lowering water arm")
-  servo.enable()
-  servo1.pulse(DOWN)
-  time.sleep(2)
-  servo.disable()
   
   while not GPIO.input(WET):
     print("no water found, pumping") 
@@ -66,51 +52,76 @@ def makeCoffee():
   time.sleep(1)
   GPIO.output(ON,0)
   
-  time.sleep(5)
+  print("making coffee...")
+  time.sleep(5*60)
 
-  #print("turn off coffee maker")
-  #GPIO.output(OFF, 1)
-  #time.sleep(1)
-  #GPIO.output(OFF,0)
+  print("turn off coffee maker")
+  GPIO.output(OFF, 1)
+  time.sleep(1)
+  GPIO.output(OFF,0)
 
-  #print("raising water arm")
-  #servo1.pulse(UP)
-  #time.sleep(1)
+def timeNow():
+  return datetime.datetime.now() - datetime.timedelta(hours=4)
 
+close = True
+hot = False
 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_data(self, data):
         data = json.loads(data)
-        name = data["user"]["name"]
-        text = data["text"]
-        userID = data["user"]["id"]
-        print("Tweet: ",text)
-        print("Name ",name)
+        #print(json.dumps(data, indent=4, sort_keys=True))
+        text = ""
+        try:
+          if 'direct_message' in data.keys():
+            text = data["direct_message"]["text"]
+            userID = data["direct_message"]["sender_id"]
+            #if 'sender' in data['direct_message'].keys():
+              #name = data['sender']
+              #print("Name ",name)
+            print("Message: ",text)
+        except ValueError:
+          print("decoding json has failed")
 	
-        if ("Be my friend" == text):
+        if ("Be my friend please" == text):
            twitter.create_friendship(userID)
-           message = "Welcome to the friendzone " + name
+           message = "Welcome to #IoTCoffee " #+ name
            twitter.send_direct_message(user_id=userID, text=message)
            print(name, " is now a friend")
         elif ("#IoTCoffee please" == text):
            if (twitter.get_user(userID).following):
-              timedone = datetime.datetime.now() + datetime.timedelta(minutes=15)
+              timedone = timeNow().now() + datetime.timedelta(minutes=5)
               message = "Making you coffee now, will be done at " + str(timedone)
               twitter.send_direct_message(user_id=userID, text=message)
               makeCoffee()
-              message = "Coffee is done at " + str(datetime.datetime.now())
+              message = "Coffee is done at " + str(timeNow().now())
               twitter.send_direct_message(user_id=userID, text=message)
+        elif("#IoTCoffee in 30 minutes please"):
+          if (twitter.get_user(userID).following):
+             timedone = timeNow().now() + datetime.timedelta(minutes=30)
+             message = "Making you coffee, will be done at " + str(timedone)
+             twitter.send_direct_message(user_id=userID, text=message)
+             print("waiting for 30 min delay")
+             time.sleep(30*60)
+             makeCoffee()
+             message = "Coffee is done at " + str(timeNow().now())
+             twitter.send_direct_message(user_id=userID, text=message)
+
            else:
              print("Cannot make coffee, user not followed")
+           return close
 
     def on_error(self, status_code):
         if status_code == 420:
             return False
 
-print("Starting tweet listener at ", datetime.datetime.now())
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = twitter.auth, listener=myStreamListener)
-
-myStream.filter(track=['#IoTCoffee'])
-
+print("Starting tweet listener at ", TimeNow().now())
+try:
+  myStreamListener = MyStreamListener()
+  myStream = tweepy.Stream(auth = twitter.auth, listener=myStreamListener)
+  myStream.userstream()
+  #myStream.filter(track=['#IoTCoffee'])
+except KeyboardInterrupt:
+  print("exiting IoTCoffee")
+  close = False
+  
